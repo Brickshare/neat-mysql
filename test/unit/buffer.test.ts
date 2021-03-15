dd4fa546330c5eb5dcf4e08d40ff175738f6bcfe
hoopra <hoopra12@gmail.com>
2021-03-15 12:56:50 +0100

    test: Add buffer test

---
+++ test/unit/buffer.test.ts
@@ -0,0 +1,31 @@
+import { formatBuffer, stringifyBufferValues } from '@src/buffer';
+import 'jest';
+
+describe('formatBuffer', () => {
+  it(`should return same value if it's not a buffer`, () => {
+    expect(formatBuffer('yo', '1')).toBe('1');
+  });
+
+  it(`should return buffer string value using default (binary) encoding`, () => {
+    expect(formatBuffer('yo', Buffer.from('1'))).toBe('1');
+    expect(formatBuffer('yo', Buffer.from('0'))).toBe('0');
+    expect(formatBuffer('yo', Buffer.from('101', 'binary'))).toBe('101');
+  });
+
+  it(`should return buffer string value using input encoding`, () => {
+    expect(formatBuffer('yo', Buffer.from('101', 'base64'), { encoding: 'base64' })).toBe('100=');
+  });
+
+  it(`should return buffer string value using specific encoding`, () => {
+    expect(formatBuffer('yo', Buffer.from('def', 'hex'), { specific: { yo: 'hex' } })).toBe('de');
+  });
+});
+
+describe('formatBuffer', () => {
+  it(`should format buffers values in object roots`, () => {
+    expect(stringifyBufferValues([], {})).toEqual([]);
+    expect(
+      stringifyBufferValues([{ hello: Buffer.from('you', 'ascii'), me: 'too' }], { specific: { hello: 'ascii' } })
+    ).toEqual([{ hello: 'you', me: 'too' }]);
+  });
+});
