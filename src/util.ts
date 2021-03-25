678a4e8f413a5aa5a50a2b4731d14069ae7a5a91
Rasmus Syberg <rvs@brickshare.io>
2021-03-25 19:50:17 +0100

    chore: Simplify formatSQL by rem nextLine and trim

--- src/util.ts
+++ src/util.ts
@@ -1,7 +1,13 @@
 import { Query, QueryArg, QueryObject } from './types';
 import sqlstring from 'sqlstring';
 
-export const formatSQL = (sql: string, args: QueryArg[] = []): string => sqlstring.format(sql, args);
+export const formatSQL = (sql: string, args: QueryArg[] = []): string =>
+  sqlstring
+    .format(sql, args)
+    .split('\n')
+    .filter(str => str)
+    .map(str => str.trim())
+    .join(' ');
 
 export const asQuery = (query: Query | QueryObject | string): [string] | [string, QueryArg[]] => {
   if (typeof query === 'string') {
